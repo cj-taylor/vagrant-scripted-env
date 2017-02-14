f6458b409f70b68c86b92e6d0642d273785c3797
CJ Taylor <cjtayl05@gmail.com>
2017-02-13 20:18:55 -0500

    Updated windows setup comments.

--- devops/windows/setup.bat
+++ devops/windows/setup.bat
@@ -1,3 +1,10 @@
 @ECHO OFF
+
+REM run our main driver script
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1'"
-PAUSE
+
+REM Switch to project dir
+cd project 
+
+REM spin up the vagrant box 
+vagrant up 
