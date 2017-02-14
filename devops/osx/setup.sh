9eae541a42cea6781a0ac70c50693403644ec1f2
CJ Taylor <cjtayl05@gmail.com>
2017-02-13 20:18:26 -0500

    Updated OSX scripts.

--- devops/osx/setup.sh
+++ devops/osx/setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-./install-app.sh http://download.virtualbox.org/virtualbox/5.1.10/VirtualBox-5.1.10-112026-OSX.dmg VirtualBox
+echo "Installing VirtualBox"
+./devops/osx/install-app.sh http://download.virtualbox.org/virtualbox/5.1.10/VirtualBox-5.1.10-112026-OSX.dmg VirtualBox
 
-./install-app.sh https://releases.hashicorp.com/vagrant/1.9.0/vagrant_1.9.0.dmg Vagrant
+echo "Installing Vagrant"
+./devops/osx/install-app.sh https://releases.hashicorp.com/vagrant/1.9.0/vagrant_1.9.0.dmg Vagrant
 
-cd ../../project 
+cd project
 vagrant up 
